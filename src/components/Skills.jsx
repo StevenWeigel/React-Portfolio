65c231113896ec9716f8493442759655a516ce98
Steven Weigel <steveo.j.weigel@gmail.com>
2024-10-27 15:37:38 -0500

    Updating skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import CLogo from '../assets/c_code.png'
 import CPPLogo from '../assets/cpp_code.png'
-import ReactImg from '../assets/react.png'
 import Python from '../assets/python.png'
-import Golang from '../assets/golang.png'
 import Java from '../assets/java.png'
 
 const Skills = () => {
@@ -14,10 +12,10 @@ const Skills = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-[#059e5e]'>Skills</p>
-                <p className='py-4 text-lg'>These are the languages / frameworks that I have the most experience with:</p>
+                <p className='py-4 text-lg'>These are the programming languages that I have the most experience with:</p>
             </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-6 text-center py-8 duration-1000'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-2 gap-6 text-center py-8 duration-1000'>
                 <div className='rounded-xl shadow-md shadow-[#050d17] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={CLogo} alt="C icon" />
                     <p className='my-3'>C</p>
